825506ebbb97215cb074e4a52a0fe04bba358ac2
DanDragomir73 <102732365+DanDragomir73@users.noreply.github.com>
2022-04-06 23:25:02 +0100

    Add files via upload

---
+++ FirstApp/DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int : {0} ", sizeof(int));
+            Console.WriteLine("Size of int : {0} ", sizeof(long));
+            Console.WriteLine("Size of int : {0} ", sizeof(float));
+            Console.WriteLine("Size of int : {0} ", sizeof(double));
+            Console.WriteLine("Size of int : {0} ", sizeof(decimal));
+            Console.WriteLine("Size of int : {0} ", sizeof(bool));
+            Console.WriteLine("Size of int : {0} ", sizeof(char));
+            Console.WriteLine("Size of int : {0} ", sizeof(byte));
+            Console.ReadLine();
+
+        }
+
+    }
+}
