113ce0b9fbcc634343789bea76ccf664ad87cd31
DanDragomir73 <102732365+DanDragomir73@users.noreply.github.com>
2022-04-06 23:27:49 +0100

    Add files via upload

---
+++ Inheritance/Inheritance/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Account : Person
+    {
+        public Account() { }
+        public double getAccountBalance()
+        {
+            return currentAccount;
+        }
+        public double setAccountBalance(double amount)
+        {
+            currentAccount = currentAccount + amount;
+            return currentAccount;
+        }
+        public double witdrawFromAccountBalance(double amountToWithDraw)
+        {
+            currentAccount = currentAccount - amountToWithDraw;
+            return currentAccount;
+        }
+
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate account class
+            Account myAccount = new Account();
+            double currentAmount = 0.0;
+            //save money in current account
+            currentAmount = myAccount.setAccountBalance(1000.00);
+            Console.WriteLine("current amount : {0}", currentAmount);
+
+            //withdraw 200.00
+            currentAmount = myAccount.witdrawFromAccountBalance(200.00);
+            Console.WriteLine("current amount : {0}",currentAmount);
+
+            //get balance
+            currentAmount = myAccount.getAccountBalance();
+            Console.WriteLine("current amount : {0}",currentAmount);
+            Console.ReadLine();
+        }
+    }
+}
