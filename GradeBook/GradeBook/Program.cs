21b09a7f9b17b2619cf0dd6f276f2cb2aca962b3
DanDragomir73 <102732365+DanDragomir73@users.noreply.github.com>
2022-04-06 23:36:11 +0100

    Add files via upload

---
+++ GradeBook/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var x = 45.65;
+                var y = 33.22;
+                var result = x + y;
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
